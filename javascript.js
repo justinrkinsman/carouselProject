626c783007333a77ee48a7fb89e6ddac163c2690
Justin <justinrkinsman@gmail.com>
2022-07-14 11:36:51 -0300

    Add carousel layout to page

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let myFunction = (function(e) {
+    let index = e.target.classList[0].slice(-1)
+    let div = document.getElementById(`myDropdown${index}`)
+    div.classList.toggle('show')
+    return index
+})
+
+window.onclick = function(event) {
+    if (!event.target.matches(`button`)) {
+        let dropdowns = document.getElementsByClassName('dropdown-contentX');
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
